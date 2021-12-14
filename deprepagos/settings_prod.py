c3135120b9d954a0b6e030e40668e27106d4b8ad
Mauro Spivak <mauros@gmail.com>
2021-12-14 18:16:31 -0300

    use tls

--- deprepagos/settings_prod.py
+++ deprepagos/settings_prod.py
@@ -6,6 +6,7 @@ EMAIL_HOST = os.environ.get('EMAIL_HOST')
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 EMAIL_PORT = os.environ.get('EMAIL_PORT')
+EMAIL_USE_TLS = True
 
 MERCADOPAGO = {
     'PUBLIC_KEY': os.environ.get('MERCADOPAGO_PUBLIC_KEY'),
