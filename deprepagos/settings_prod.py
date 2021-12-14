7dc1261432f44ab7c82f0cad250d03b8e68d371c
Mauro Spivak <mauros@gmail.com>
2021-12-14 12:25:46 -0300

    prod settings

---
+++ deprepagos/settings_prod.py
@@ -0,0 +1,8 @@
+from deprepagos.settings import *
+
+STATICFILES_STORAGE = 'deprepagos.storages.S3PipelineManifestStorage'
+
+MERCADOPAGO = {
+    'PUBLIC_KEY': 'APP_USR-3c0fb6b2-ce7d-48cc-b8be-d7242144a25c',
+    'ACCESS_TOKEN': 'APP_USR-8395362091404017-102216-3563fee7c4d127a7f4c1a80982f56596-663579293',
+}
