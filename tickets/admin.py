de916bf7b9023a6f39ed4208950897fd797b9c4c
Iván Raskovsky <raskovsky+git@gmail.com>
2021-12-15 15:19:31 -0300

    admin stuff

--- tickets/admin.py
+++ tickets/admin.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.contrib import admin
 from django.urls import reverse
 from django.utils.safestring import mark_safe
@@ -26,7 +27,24 @@ class OrderAdmin(admin.ModelAdmin):
             order.ticket_type.name
         ))
 
+class OrdersInline(admin.StackedInline):
+    model = Order
+    fields = ('first_name', 'last_name', 'email', 'amount', 'status', )
+    show_change_link = True
+
+class CouponAdmin(admin.ModelAdmin):
+    list_display = ('token', 'ticket_type', 'max_tickets', 'tickets_remaining', 'display_url', )
+    readonly_fields = ('tickets_remaining', 'display_url', )
+    inlines = [OrdersInline, ]
+
+    class Meta:
+        model = Coupon
+        fields = ('display_url', )
+
+    def display_url(self, order):
+        return f"{getattr(settings, 'APP_URL')}/?coupon={order.token}"
+
 admin.site.register(Ticket, TicketAdmin)
 admin.site.register(TicketType, TicketTypeAdmin)
 admin.site.register(Order, OrderAdmin)
-admin.site.register(Coupon)
+admin.site.register(Coupon, CouponAdmin)
