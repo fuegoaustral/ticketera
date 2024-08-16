1fbf97a51bcd5774ad3e4bcca86259c41fff0907
guillermodoghel <guillermo@paella.dev>
2024-08-16 20:11:29 -0300

    ENVS

--- deprepagos/settings.py
+++ deprepagos/settings.py
@@ -3,18 +3,20 @@ import os
 
 import django
 from django.utils.encoding import smart_str
+
 django.utils.encoding.smart_text = smart_str
 
 import django.utils.translation as original_translation
 from django.utils.translation import gettext_lazy
+
 original_translation.ugettext_lazy = gettext_lazy
 
 from django.utils.encoding import force_str
+
 django.utils.encoding.force_text = force_str
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -24,7 +26,6 @@ SECRET_KEY = 'django-insecure-s$(*=)6^h$p=d6e4tpv#-s7_hg&cl!vc@yzas371ubj=+ks&cc
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'True') == 'True'
-
 
 ALLOWED_HOSTS = [
     '127.0.0.1',
@@ -45,7 +46,6 @@ AUTHENTICATION_BACKENDS = [
     'allauth.account.auth_backends.AuthenticationBackend',
 
 ]
-
 
 INSTALLED_APPS = [
     'django.contrib.auth',
@@ -93,7 +93,7 @@ TEMPLATES = [
         'DIRS': [
             os.path.join(BASE_DIR, 'tickets/templates'),
 
-],
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -109,7 +109,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'deprepagos.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
@@ -123,7 +122,6 @@ DATABASES = {
         'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
@@ -143,7 +141,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
@@ -156,7 +153,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
@@ -205,7 +201,7 @@ MERCADOPAGO = {
     # 'ACCESS_TOKEN': 'TEST-3993191188804171-120900-49b84931b82af80f4e67442917d5a311-2309703',
     # 'PUBLIC_KEY': 'TEST-adcf53df-bf76-4579-8f85-e3e1ef658c1c', #
     # 'ACCESS_TOKEN': 'TEST-8395362091404017-102216-655293c3d37f873676196ce190a66889-663579293',
-    'PUBLIC_KEY': 'TEST-467cbbca-1aac-4d7f-be0b-53bcf92a3064', # test_user_82107219@testuser.com / qatest8011
+    'PUBLIC_KEY': 'TEST-467cbbca-1aac-4d7f-be0b-53bcf92a3064',  # test_user_82107219@testuser.com / qatest8011
     'ACCESS_TOKEN': 'TEST-6630578586763408-121117-afe84675e0d0a70b7c67a1ade5909b2c-1037325933',
 }
 
@@ -224,18 +220,29 @@ TWILIO_ACCOUNT_SID = os.environ.get('TWILIO_ACCOUNT_SID', '')
 TWILIO_AUTH_TOKEN = os.environ.get('TWILIO_AUTH_TOKEN', '')
 TWILIO_VERIFY_SERVICE_SID = os.environ.get('TWILIO_VERIFY_SERVICE_SID', '')
 
+SOCIALACCOUNT_PROVIDERS = {
+    'google': {
+        'SCOPE': [
+            'email',
+        ],
+        'APP': {
+            'client_id': os.environ.get('GOOGLE_CLIENT_ID', ''),
+            'secret': os.environ.get('GOOGLE_SECRET', '')
+        }
+    }
+}
 
 # user comprador {"id":1037327132,"nickname":"TETE9670391","password":"qatest8330","site_status":"active","email":"test_user_43578812@testuser.com"}%
 # user comprador {"id":1037346624,"nickname":"TETE9234065","password":"qatest9033","site_status":"active","email":"test_user_72163657@testuser.com"}%
 
 try:
     from deprepagos.local_settings import *
+
     INSTALLED_APPS.extend(EXTRA_INSTALLED_APPS)
 except ImportError:
     # using print and not log here as logging is yet not configured
     print('local settings not found')
     pass
-
 
 LOGGING = {
     'version': 1,
@@ -271,8 +278,7 @@ ACCOUNT_AUTHENTICATION_METHOD = 'email'  # 'username_email', 'username'
 ACCOUNT_EMAIL_CONFIRMATION_EXPIRE_DAYS = 3
 
 SOCIALACCOUNT_EMAIL_AUTHENTICATION = True
-SOCIALACCOUNT_LOGIN_ON_GET=True
-ACCOUNT_LOGOUT_ON_GET=True
-ACCOUNT_CONFIRM_EMAIL_ON_GET=True
+SOCIALACCOUNT_LOGIN_ON_GET = True
+ACCOUNT_LOGOUT_ON_GET = True
+ACCOUNT_CONFIRM_EMAIL_ON_GET = True
 ACCOUNT_EMAIL_CONFIRMATION_ANONYMOUS_REDIRECT_URL = '/accounts/verification-congrats/'
-
