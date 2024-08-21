2b2ac5f530fc0e210d4d562526430dfb891badf6
guillermodoghel <guillermo@paella.dev>
2024-08-21 02:18:59 -0300

    Major checkout refactor

--- tickets/views/utils.py
+++ tickets/views/utils.py
@@ -1,7 +1,11 @@
+from django.db.models import Sum
 from django.http import HttpResponseRedirect
 
+from events.models import Event
 from tickets.models import TicketType, Order
 import logging
+
+
 def is_order_valid(order):
     num_tickets = order.ticket_set.count()
     ticket_types = TicketType.objects.get_available(order.coupon, order.ticket_type.event)
@@ -21,6 +25,7 @@ def is_order_valid(order):
         return False
     return True
 
+
 def _complete_order(order):
     logging.info('completing order')
     order.status = Order.OrderStatus.CONFIRMED
@@ -29,6 +34,22 @@ def _complete_order(order):
     logging.info('redirecting')
     return HttpResponseRedirect(order.get_resource_url())
 
+
 def available_tickets_for_user(user):
-    # Implement the logic to calculate the available tickets for the user
-    pass
+    from tickets.models import Order
+
+    try:
+        event = Event.objects.get(active=True)
+    except Event.DoesNotExist:
+        return 0
+
+    # Sum the quantity of tickets in confirmed orders for this event and user
+    tickets_bought = (Order.objects
+                      .filter(email=user.email)
+                      .filter(order_tickets__ticket_type__event=event)
+                      .filter(status=Order.OrderStatus.CONFIRMED)
+                      .annotate(total_quantity=Sum('order_tickets__quantity'))
+                      .aggregate(tickets_bought=Sum('total_quantity'))
+                      )['tickets_bought'] or 0
+
+    return event.max_tickets_per_order - tickets_bought
