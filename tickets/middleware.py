9473f9fec02b4e84b0f145ec5a42476353e68ebf
guillermodoghel <guillermo@paella.dev>
2024-08-29 01:46:05 -0300

    Actual emails

--- tickets/middleware.py
+++ tickets/middleware.py
@@ -1,5 +1,7 @@
 from django.shortcuts import redirect
 from django.urls import reverse
+from django.utils.deprecation import MiddlewareMixin
+
 
 class ProfileCompletionMiddleware:
     def __init__(self, get_response):
@@ -10,7 +12,14 @@ class ProfileCompletionMiddleware:
         if request.user.is_authenticated and not request.user.is_staff:
             # Allow access to logout URL and complete_profile URL
             if not request.path.startswith(reverse('account_logout')) and \
-               not request.path.startswith(reverse('complete_profile')):
+                    not request.path.startswith(reverse('complete_profile')):
                 if request.user.profile.profile_completion != 'COMPLETE':
                     return redirect('complete_profile')
         return self.get_response(request)
+
+
+class DeviceDetectionMiddleware(MiddlewareMixin):
+    def process_request(self, request):
+        user_agent = request.META.get('HTTP_USER_AGENT', '').lower()
+        request.is_iphone = 'iphone' in user_agent
+        request.is_android = 'android' in user_agent
