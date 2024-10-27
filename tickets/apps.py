0da02204b386e2d6cabf3cddb3345b3a798897e1
Iván Raskovsky <raskovsky+git@gmail.com>
2024-10-27 15:50:53 -0300

    move profile model to profile app

--- tickets/apps.py
+++ tickets/apps.py
@@ -4,6 +4,3 @@ from django.apps import AppConfig
 class TicketsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'tickets'
-
-    def ready(self):
-        import tickets.signals
