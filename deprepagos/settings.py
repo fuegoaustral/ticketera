6de7cd14d421683ca8be459d322a39a97e9d631e
Iván Raskovsky <raskovsky+git@gmail.com>
2024-10-28 12:43:36 -0300

    new design for all account views

--- deprepagos/settings.py
+++ deprepagos/settings.py
@@ -98,7 +98,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            os.path.join(BASE_DIR, 'tickets/templates'),
+            os.path.join(BASE_DIR, 'user_profile/templates'),
 
         ],
         'APP_DIRS': True,
