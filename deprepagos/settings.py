ce92cfa7f01ac93d752ca15a3fbced4f992626c1
guillermodoghel <guillermo@paella.dev>
2024-11-12 01:22:05 -0300

    logging amd email cleanup

--- deprepagos/settings.py
+++ deprepagos/settings.py
@@ -82,12 +82,12 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'utils.loggerMiddleware.LoggerMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'utils.loggerMiddleware.LoggerMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'auditlog.middleware.AuditlogMiddleware',
