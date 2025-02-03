8b5a87b80257d3351860c03b782b98f176e825c3
Viktor <67437+vfonic@users.noreply.github.com>
2025-02-03 17:02:36 -0300

    Pre/post event forms: Minify ENV vars

--- tickets/views/pre_post_event.py
+++ tickets/views/pre_post_event.py
@@ -5,24 +5,24 @@ from django.template import loader
 
 @login_required()
 def ingreso_anticipado(request):
-    context = { 'form_url': f"https://docs.google.com/forms/u/0/d/e/{os.environ.get('INGRESO_ANTICIPADO_FORM_ID')}/formResponse" }
+    context = { 'form_url': f"https://docs.google.com/forms/u/0/d/e/1FAIpQLSfO2H29d6QY_K7QIp7A4s1-{os.environ.get('PRE_ID')}/formResponse" }
     template = loader.get_template('pre_post_event/ingreso_anticipado.html')
     return HttpResponse(template.render(context, request))
 
 @login_required()
 def ingreso_anticipado_proveedores(request):
-    context = { 'form_url': f"https://docs.google.com/forms/u/0/d/e/{os.environ.get('INGRESO_ANTICIPADO_PROVEEDORES_FORM_ID')}/formResponse" }
+    context = { 'form_url': f"https://docs.google.com/forms/u/0/d/e/1FAIpQLSddybkyHuel2SXtsRo75HScR25lEcN5bpbwMAkx_{os.environ.get('PRE_PROV_ID')}/formResponse" }
     template = loader.get_template('pre_post_event/ingreso_anticipado_proveedores.html')
     return HttpResponse(template.render(context, request))
 
 @login_required()
 def late_checkout(request):
-    context = { 'form_url': f"https://docs.google.com/forms/u/0/d/e/{os.environ.get('LATE_CHECKOUT_FORM_ID')}/formResponse" }
+    context = { 'form_url': f"https://docs.google.com/forms/u/0/d/e/1FAIpQLScwYW8fpTss_ia2KZ5ggdyVm1DUiDO8mvZ{os.environ.get('LCO_ID')}/formResponse" }
     template = loader.get_template('pre_post_event/late_checkout.html')
     return HttpResponse(template.render(context, request))
 
 @login_required()
 def late_checkout_proveedores(request):
-    context = { 'form_url': f"https://docs.google.com/forms/u/0/d/e/{os.environ.get('LATE_CHECKOUT_PROVEEDORES_FORM_ID')}/formResponse" }
+    context = { 'form_url': f"https://docs.google.com/forms/u/0/d/e/1FAIpQLSd_5_fCJOYXUuKbab41oUwMMW-{os.environ.get('LCO_PROV_ID')}/formResponse" }
     template = loader.get_template('pre_post_event/late_checkout_proveedores.html')
     return HttpResponse(template.render(context, request))
