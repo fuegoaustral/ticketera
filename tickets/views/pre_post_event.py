b6e8f4e0516cfde6cf1252441a882ae8d0fffd25
Viktor <67437+vfonic@users.noreply.github.com>
2025-02-03 14:01:37 -0300

    Pre/post event form: Use form ID instead of form URL

--- tickets/views/pre_post_event.py
+++ tickets/views/pre_post_event.py
@@ -5,24 +5,24 @@ from django.template import loader
 
 @login_required()
 def ingreso_anticipado(request):
-    context = { 'form_url': os.environ.get('INGRESO_ANTICIPADO_FORM_URL') }
+    context = { 'form_url': f"https://docs.google.com/forms/u/0/d/e/{os.environ.get('INGRESO_ANTICIPADO_FORM_ID')}/formResponse" }
     template = loader.get_template('pre_post_event/ingreso_anticipado.html')
     return HttpResponse(template.render(context, request))
 
 @login_required()
 def ingreso_anticipado_proveedores(request):
-    context = { 'form_url': os.environ.get('INGRESO_ANTICIPADO_PROVEEDORES_FORM_URL') }
+    context = { 'form_url': f"https://docs.google.com/forms/u/0/d/e/{os.environ.get('INGRESO_ANTICIPADO_PROVEEDORES_FORM_ID')}/formResponse" }
     template = loader.get_template('pre_post_event/ingreso_anticipado_proveedores.html')
     return HttpResponse(template.render(context, request))
 
 @login_required()
 def late_checkout(request):
-    context = { 'form_url': os.environ.get('LATE_CHECKOUT_FORM_URL') }
+    context = { 'form_url': f"https://docs.google.com/forms/u/0/d/e/{os.environ.get('LATE_CHECKOUT_FORM_ID')}/formResponse" }
     template = loader.get_template('pre_post_event/late_checkout.html')
     return HttpResponse(template.render(context, request))
 
 @login_required()
 def late_checkout_proveedores(request):
-    context = { 'form_url': os.environ.get('LATE_CHECKOUT_PROVEEDORES_FORM_URL') }
+    context = { 'form_url': f"https://docs.google.com/forms/u/0/d/e/{os.environ.get('LATE_CHECKOUT_PROVEEDORES_FORM_ID')}/formResponse" }
     template = loader.get_template('pre_post_event/late_checkout_proveedores.html')
     return HttpResponse(template.render(context, request))
