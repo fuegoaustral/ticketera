17bb44e24daf8dc1e51409ddece54f9ee875a072
Viktor <67437+vfonic@users.noreply.github.com>
2025-02-04 11:17:51 -0300

    Pre/post event forms: Submit currently logged in user's email

--- tickets/views/pre_post_event.py
+++ tickets/views/pre_post_event.py
@@ -5,24 +5,36 @@ from django.template import loader
 
 @login_required()
 def ingreso_anticipado(request):
-    context = { 'form_url': f"https://docs.google.com/forms/u/0/d/e/1FAIpQLSfO2H29d6QY_K7QIp7A4s1-wX3IMCLQPH7cE7LLuLe8p3{os.environ.get('PRE_ID')}/formResponse" }
+    context = {
+        'form_url': f"https://docs.google.com/forms/u/0/d/e/1FAIpQLSfO2H29d6QY_K7QIp7A4s1-wX3IMCLQPH7cE7LLuLe8p3{os.environ.get('PRE_ID')}/formResponse",
+        'user': request.user,
+    }
     template = loader.get_template('pre_post_event/ingreso_anticipado.html')
     return HttpResponse(template.render(context, request))
 
 @login_required()
 def ingreso_anticipado_proveedores(request):
-    context = { 'form_url': f"https://docs.google.com/forms/u/0/d/e/1FAIpQLSddybkyHuel2SXtsRo75HScR25lEcN5bpbwMAkx_LKAbR{os.environ.get('PRE_PROV_ID')}/formResponse" }
+    context = {
+        'form_url': f"https://docs.google.com/forms/u/0/d/e/1FAIpQLSddybkyHuel2SXtsRo75HScR25lEcN5bpbwMAkx_LKAbR{os.environ.get('PRE_PROV_ID')}/formResponse",
+        'user': request.user,
+    }
     template = loader.get_template('pre_post_event/ingreso_anticipado_proveedores.html')
     return HttpResponse(template.render(context, request))
 
 @login_required()
 def late_checkout(request):
-    context = { 'form_url': f"https://docs.google.com/forms/u/0/d/e/1FAIpQLScwYW8fpTss_ia2KZ5ggdyVm1DUiDO8mvZwzym8B0F87P{os.environ.get('LCO_ID')}/formResponse" }
+    context = {
+        'form_url': f"https://docs.google.com/forms/u/0/d/e/1FAIpQLScwYW8fpTss_ia2KZ5ggdyVm1DUiDO8mvZwzym8B0F87P{os.environ.get('LCO_ID')}/formResponse",
+        'user': request.user,
+    }
     template = loader.get_template('pre_post_event/late_checkout.html')
     return HttpResponse(template.render(context, request))
 
 @login_required()
 def late_checkout_proveedores(request):
-    context = { 'form_url': f"https://docs.google.com/forms/u/0/d/e/1FAIpQLSd_5_fCJOYXUuKbab41oUwMMW-IyKR1bdsky41CY40VYQ{os.environ.get('LCO_PROV_ID')}/formResponse" }
+    context = {
+        'form_url': f"https://docs.google.com/forms/u/0/d/e/1FAIpQLSd_5_fCJOYXUuKbab41oUwMMW-IyKR1bdsky41CY40VYQ{os.environ.get('LCO_PROV_ID')}/formResponse",
+        'user': request.user,
+    }
     template = loader.get_template('pre_post_event/late_checkout_proveedores.html')
     return HttpResponse(template.render(context, request))
